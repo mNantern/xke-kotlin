d30a8bf3958fc8b07329d24c74e622a09f220c31
Matthieu Nantern <mnantern@xebia.fr>
2016-03-22 21:43:34 +0100

    Create Exercise

--- src/main/kotlin/fr/xebia/xke-kotlin/Data.kt
+++ src/main/kotlin/fr/xebia/xke-kotlin/Data.kt
@@ -1,6 +1,4 @@
 package fr.xebia.xke
 
-import java.time.Instant
-import java.util.*
 
-data class Data(val smartphoneId: UUID, val type: String, val eventTime: Instant, val value: String)
+//TODO !
--- src/main/kotlin/fr/xebia/xke-kotlin/DataController.kt
+++ src/main/kotlin/fr/xebia/xke-kotlin/DataController.kt
@@ -1,18 +1,9 @@
 package fr.xebia.xke
 
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class DataController {
 
-    @RequestMapping("/data")
-    @ResponseStatus(value = HttpStatus.CREATED)
-    fun create(@RequestBody dataList: List<Data>) {
-        println("${dataList.size} elements in POST /data:")
-        dataList.forEach { println(it.toString()) }
-    }
+    //TODO
 }
